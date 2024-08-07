da27fd75af3604aa2123bdcc2da572fe23dd3761
Gino Heyman <gino.heyman@gmail.com>
2024-08-07 20:33:17 +0200

    feat: get torrent file info

--- app/main.ts
+++ app/main.ts
@@ -1,13 +1,30 @@
+import { readFileSync } from "node:fs";
 import { decode } from "./bencode";
 
 const args = process.argv;
-const bencodedValue = args[3];
 
-if (args[2] === "decode") {
-  try {
-    const decoded = decode(Buffer.from(bencodedValue));
-    console.log(typeof decode == "number" ? decoded : JSON.stringify(decoded));
-  } catch (error: any) {
-    console.error(error.message);
+switch (args[2]) {
+  case "decode": {
+    try {
+      const decoded = decode(Buffer.from(args[3]));
+      console.log(
+        typeof decode == "number" ? decoded : JSON.stringify(decoded)
+      );
+    } catch (error: any) {
+      console.error(error.message);
+    }
+  }
+  case "info": {
+    try {
+      const decoded = decode(readFileSync(args[3])) as Map<string, any>;
+
+      console.log(
+        `Tracker URL: ${decoded.get("announce")?.toString()}\nLength: ${decoded
+          .get("info")
+          .get("length")}`
+      );
+    } catch (error: any) {
+      console.error(error.message);
+    }
   }
 }
