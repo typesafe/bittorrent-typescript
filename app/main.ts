f0b87a6f485da93503f6f8bb2fac64d4b4a50744
Gino Heyman <gino.heyman@gmail.com>
2024-08-07 20:08:14 +0200

    feat: don't JSON stringfy decoded numbers

--- app/main.ts
+++ app/main.ts
@@ -6,7 +6,7 @@ const bencodedValue = args[3];
 if (args[2] === "decode") {
   try {
     const decoded = decode(Buffer.from(bencodedValue));
-    console.log(JSON.stringify(decoded.toString()));
+    console.log(typeof decode == "number" ? decoded : JSON.stringify(decoded));
   } catch (error: any) {
     console.error(error.message);
   }
